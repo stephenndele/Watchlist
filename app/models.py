dd24ad9f92c990eed1904d65785e6e04f3330866
Stephen Ndele <stephenndele09346@gmail.com>
2021-02-24 15:42:37 +0300

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from . import db
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -13,16 +23,28 @@ class Movie:
 
 
 
-class Review:
+# class Review:
 
-    all_reviews = []
+#     all_reviews = []
 
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
+#     def __init__(self,movie_id,title,imageurl,review):
+#         self.movie_id = movie_id
+#         self.title = title
+#         self.imageurl = imageurl
+#         self.review = review
 
+
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer,primary_key = True)
+    movie_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    image_path = db.Column(db.String)
+    movie_review = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
     def save_review(self):
         Review.all_reviews.append(self)
@@ -42,3 +64,40 @@ class Review:
                 response.append(review)
 
         return response
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    # pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+    def __repr__(self):
+        return f'User {self.name}'
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY='bc581fe13512e9ec017f976c0579c526'
 export SECRET_KEY='stephenndele'
+export MAIL_USERNAME='stephenndele093@gmail.com'
+export MAIL_PASSWORD='Newbeggining2020!'
 
 python3 manage.py server
